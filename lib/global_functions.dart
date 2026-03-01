edeab4f79b917ae26bf755248251012635bc1808
cygnet <cygnet3@proton.me>
2026-03-01 01:50:20 +0100

    feat: allow wallet creation without internet access

--- lib/global_functions.dart
+++ lib/global_functions.dart
@@ -202,6 +202,13 @@ void goToScreen(BuildContext context, Widget screen) {
   Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
 }
 
+void goToScreenAndResetPath(BuildContext context, Widget screen) {
+  Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => screen),
+      (Route<dynamic> route) => false);
+}
+
 void goBack(BuildContext context) {
   Navigator.of(context).pop();
 }
