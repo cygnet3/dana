f4c3cd7d2eb17fbf2a5941dd4ccf4efa743a3682
cygnet <cygnet3@proton.me>
2026-03-05 14:49:14 +0100

    feat: reset wallet to birthday
    Allow resetting the wallet to its birthday, which drops all stored
    transaction data.

--- lib/states/wallet_state.dart
+++ lib/states/wallet_state.dart
@@ -188,9 +188,20 @@ class WalletState extends ChangeNotifier {
     }
   }
 
-  Future<void> resetToSyncHeight(int height) async {
-    lastSync = height;
+  Future<void> resetToBirthday() async {
+    // when we reset to the birthday, clear out all stored data
+    await walletRepository.saveHistory(TxHistory.empty());
+    await walletRepository.saveOwnedOutputs(OwnedOutputs.empty());
 
+    // the sync service will handle setting the lastSync to the birthday height
+    await walletRepository.saveLastSync(null);
+
+    await _updateWalletState();
+    notifyListeners();
+  }
+
+  Future<void> resetToSyncHeight(int height) async {
+    // note: this feature is not stable, as it does not take output spent status into account
     ownedOutputs.resetToHeight(height: height);
     txHistory.resetToHeight(height: height);
 
