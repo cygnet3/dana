64d3ddf0f048312445eda11c9d7ee0236c95d210
cygnet <cygnet3@proton.me>
2026-03-18 11:30:02 +0100

    refactor: allow generic widget in circular_icon

--- lib/widgets/icons/circular_icon.dart
+++ lib/widgets/icons/circular_icon.dart
@@ -1,28 +1,18 @@
-import 'package:bitcoin_ui/bitcoin_ui.dart';
 import 'package:danawallet/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class CircularIcon extends StatelessWidget {
-  final String iconPath;
-  final double? iconHeight;
+  final Widget icon;
   final double radius;
   final Color color;
 
   const CircularIcon(
       {super.key,
-      required this.iconPath,
+      required this.icon,
       required this.radius,
-      this.iconHeight,
       this.color = danaBlue});
   @override
   Widget build(BuildContext context) {
-    final icon = SvgPicture.asset(
-      height: iconHeight,
-      iconPath,
-      colorFilter: ColorFilter.mode(Bitcoin.white, BlendMode.srcIn),
-    );
-
     return CircleAvatar(
       radius: radius,
       backgroundColor: color,
--- lib/widgets/info_widget.dart
+++ lib/widgets/info_widget.dart
@@ -2,6 +2,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:bitcoin_ui/bitcoin_ui.dart';
 import 'package:danawallet/widgets/icons/circular_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class InfoWidget extends StatelessWidget {
   final String iconPath;
@@ -17,11 +18,17 @@ class InfoWidget extends StatelessWidget {
       required this.group});
   @override
   Widget build(BuildContext context) {
+    final icon = SvgPicture.asset(
+      height: null,
+      iconPath,
+      colorFilter: ColorFilter.mode(Bitcoin.white, BlendMode.srcIn),
+    );
+
     return Row(
       children: [
         CircularIcon(
           radius: 25,
-          iconPath: iconPath,
+          icon: icon,
         ),
         const SizedBox(
           width: 20,
