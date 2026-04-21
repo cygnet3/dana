a904d996e299a8d0121aa13cf8b402a48364c0fb
cygnet <cygnet3@proton.me>
2026-04-21 16:44:37 +0200

    style: fix indentation

--- lib/screens/home/wallet/transaction_note_screen.dart
+++ lib/screens/home/wallet/transaction_note_screen.dart
@@ -58,44 +58,44 @@ class _TransactionNoteScreenState extends State<TransactionNoteScreen> {
   @override
   Widget build(BuildContext context) {
     return ScreenSkeleton(
-      showBackButton: true,
-      title: 'Enter note',
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 16),
-          TextField(
-            controller: _controller,
-            maxLength: maxChars,
-            maxLines: 3,
-            decoration: InputDecoration(
-              hintText: 'Things like who sent you this and for what?',
-              hintStyle: BitcoinTextStyle.body4(Bitcoin.neutral5),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(color: Bitcoin.neutral3),
+        showBackButton: true,
+        title: 'Enter note',
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 16),
+            TextField(
+              controller: _controller,
+              maxLength: maxChars,
+              maxLines: 3,
+              decoration: InputDecoration(
+                hintText: 'Things like who sent you this and for what?',
+                hintStyle: BitcoinTextStyle.body4(Bitcoin.neutral5),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(color: Bitcoin.neutral3),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(color: Bitcoin.neutral3),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(color: Bitcoin.neutral5),
+                ),
+                counterText: '',
               ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(color: Bitcoin.neutral3),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(color: Bitcoin.neutral5),
-              ),
-              counterText: '',
+              style: BitcoinTextStyle.body4(Bitcoin.neutral8),
             ),
-            style: BitcoinTextStyle.body4(Bitcoin.neutral8),
-          ),
-          const SizedBox(height: 8),
-          Center(
-            child: Text(
-              '$_charsRemaining/$maxChars chars remaining.',
-              style: BitcoinTextStyle.body5(Bitcoin.neutral5),
+            const SizedBox(height: 8),
+            Center(
+              child: Text(
+                '$_charsRemaining/$maxChars chars remaining.',
+                style: BitcoinTextStyle.body5(Bitcoin.neutral5),
+              ),
             ),
-          ),
-        ],
-      ),
-      footer: FooterButton(title: 'Save', onPressed: _saveNote));
+          ],
+        ),
+        footer: FooterButton(title: 'Save', onPressed: _saveNote));
   }
 }
