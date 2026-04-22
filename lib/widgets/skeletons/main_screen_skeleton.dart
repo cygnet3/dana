3675ce7162ee658a64a52772758ace0d65267d3c
cygnet <cygnet3@proton.me>
2026-04-22 14:29:38 +0200

    feat: use main screen skeleton for wallet overview screen

--- lib/widgets/skeletons/main_screen_skeleton.dart
+++ lib/widgets/skeletons/main_screen_skeleton.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 
 /// Common layout structure for all settings screens
 class MainScreenSkeleton extends StatelessWidget {
-  final String title;
   final Widget body;
+  final String? title;
   final Widget? footer;
   final AppBar? appBar;
   final VoidCallback? floatingAction;
 
   const MainScreenSkeleton({
     super.key,
-    required this.title,
+    this.title,
     required this.body,
     this.footer,
     this.appBar,
@@ -33,21 +33,24 @@ class MainScreenSkeleton extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Title
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Bitcoin.neutral8,
+          if (title != null)
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+              child: Text(
+                title!,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Bitcoin.neutral8,
+                ),
               ),
             ),
-          ),
           // Main content
           Expanded(
               child: Padding(
-                  padding: const EdgeInsetsGeometry.all(10), child: body)),
+                  padding: const EdgeInsetsGeometry.symmetric(
+                      horizontal: 20, vertical: 5),
+                  child: body)),
           // Optional footer
           if (footer != null) footer!,
         ],
