bf04ae1020cc21960cb79323795c0af47a6add93
cygnet <cygnet3@proton.me>
2026-08-06 14:02:15 +0200

    refactor: drop unused example address constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -34,10 +34,6 @@ const Duration staleExchangeRateThreshold = Duration(hours: 1);
 // colors
 const Color danaBlue = Color.fromARGB(255, 10, 109, 214);
 
-// example address, used in onboarding flow
-const String exampleAddress =
-    "sp1qq0cygnetgn3rz2kla5cp05nj5uetlsrzez0l4p8g7wehf7ldr93lcqadw65upymwzvp5ed38l8ur2rznd6934xh95msevwrdwtrpk372hyz4vr6g";
-
 // number of decimals in 1 btc
 const int bitcoinUnits = 8;
 // String that displays when amount is hidden
